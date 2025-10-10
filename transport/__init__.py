6121bc4753eeba126083b4869f1f7a495b9f7c91
Char <156957455+Charisma-Ricarte@users.noreply.github.com>
2025-10-09 21:16:32 -0500

    Delete transport directory

--- transport/__init__.py
+++
@@ -1,8 +0,0 @@
-# transport/__init__.py
-
-# Expose main classes/functions for easy import
-from .packet import make_packet, parse_packet
-from .sr import SRConnection
-from .connection import Connection
-
-__all__ = ["make_packet", "parse_packet", "SRConnection", "Connection"]
