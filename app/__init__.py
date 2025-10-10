94a01a1b0cf5c016e4faf1ca5c2fa6de73dbc21d
Char <156957455+Charisma-Ricarte@users.noreply.github.com>
2025-10-09 21:15:06 -0500

    Delete app directory

--- app/__init__.py
+++
@@ -1,8 +0,0 @@
-# app/__init__.py
-
-# Optional: expose key app modules
-from .server import run_server
-from .client import run_client
-from .rooms import RoomManager
-
-__all__ = ["run_server", "run_client", "RoomManager"]
